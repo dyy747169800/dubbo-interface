ef5fa9460c93097954893a5ff3a33c77157d7df2
DuanYangYu <DuanYangYu@uban.com>
2017-02-20 11:47:34 +0800

    初次提交

---
+++ src/main/java/com/uban/service/DubboDemoService.java
@@ -0,0 +1,13 @@
+package com.uban.service;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * Created by DuanYangYu on 2016/11/2 0002.
+ */
+public interface DubboDemoService {
+
+    public JSONObject demo(String val);
+
+    public JSONObject getUsers();
+}
